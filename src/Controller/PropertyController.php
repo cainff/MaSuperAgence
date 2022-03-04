c7952759b1c95358f558579d2abdbc7a282f4fd5
cainff <kavitagermain@MacBook-Air-de-Kavita.local>
2022-03-04 12:38:41 +0100

    pagination et filtre de recherche

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Property;
@@ -24,12 +28,21 @@ class PropertyController extends AbstractController{
     * @return Response
     */
 
-   public function index(): Response {
+   public function index(PaginatorInterface $paginator, Request $request): Response {
 
-      $property = $this->repository->findAllVisible();
+      $search = new PropertySearch();
+      $form = $this->createForm(PropertySearchType::class, $search);
+      $form->handleRequest($request);
+
+      $properties = $paginator->paginate(
+          $this->repository->findAllVisibleQuery($search),
+          $request->query->getInt('page', 1), 12
+      );
       
       return $this->render('property/index.html.twig', [
-         'current_menu' => 'properties'
+         'current_menu' => 'properties',
+          'properties' => $properties,
+          'form' => $form->createView()
       ]);
 
    }
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -26,6 +28,30 @@ class PropertyRepository extends ServiceEntityRepository
 
     }
 
+    /**
+     * @return Query
+     */
+    public function findAllVisibleQuery(PropertySearch $search): Query{
+        $query = $this->findVisibleQuery();
+
+        if($search->getMaxPrice()){
+            $query = $query
+                ->andWhere('p.price <= :maxPrice')
+                ->setParameter('maxPrice', $search->getMaxPrice());
+        }
+
+        if($search->getMinSurface()){
+            $query = $query
+                ->andWhere('p.surface >= :minSurface')
+                ->setParameter('minSurface', $search->getMinSurface());
+        }
+
+        return $query->getQuery();
+    }
+
+    /**
+     * @return float|int|mixed|string
+     */
     public function findAllVisible(){
         return $this->createQueryBuilder('p')
             ->where('p.sold = false')
